e9b1a846c353da26ab5bd0780fb4d3aba1392c01
haitrec <haitrec@gmx.de>
2020-07-20 13:08:04 +0200

    Added ENNumber field to CapturedData.

--- coralibre-sdk/sdk/src/main/java/org/coralibre/android/sdk/internal/database/ppcp/model/CapturedData.java
+++ coralibre-sdk/sdk/src/main/java/org/coralibre/android/sdk/internal/database/ppcp/model/CapturedData.java
@@ -1,7 +1,17 @@
 package org.coralibre.android.sdk.internal.database.ppcp.model;
 
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import org.coralibre.android.sdk.internal.crypto.ppcp.ENNumber;
+
 public class CapturedData {
-    private long captureTimestamp; // in milliseconds since Epoch
+
+
+
+    private final long captureTimestamp; // in milliseconds since Epoch
+    private final ENNumber enNumber;
 
     /** in dB */
     private byte rssi;
@@ -11,12 +21,17 @@ public class CapturedData {
 
     public CapturedData(long captureTimestamp, byte rssi, byte[] payload) {
         this.captureTimestamp = captureTimestamp;
+        this.enNumber = new ENNumber(captureTimestamp, true);
         this.rssi = rssi;
         this.payload = payload;
     }
 
     public Long getCaptureTimestamp() {
         return captureTimestamp;
+    }
+
+    public ENNumber getEnNumber() {
+        return enNumber;
     }
 
     public byte getRssi() {
