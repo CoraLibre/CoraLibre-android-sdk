c3034a558567fa309604f669caf496fd5f5f4282
Björn Petersen <bjoern.petersen@cosee.biz>
2020-09-10 13:25:51 +0200

    Add JavaDoc for ResultTask

--- sdk/src/main/java/org/coralibre/android/sdk/fakegms/tasks/ResultTask.java
+++ sdk/src/main/java/org/coralibre/android/sdk/fakegms/tasks/ResultTask.java
@@ -2,6 +2,11 @@ package org.coralibre.android.sdk.fakegms.tasks;
 
 import androidx.annotation.NonNull;
 
+/**
+ * A task implementation with a predetermined result.
+ * <p>
+ * Since no computation takes place, a ResultTask is always successful.
+ */
 final class ResultTask<T> extends Task<T> {
     private final T value;
 
